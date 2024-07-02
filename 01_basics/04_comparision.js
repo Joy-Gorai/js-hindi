bd00cf40dcf818b0f92c5eac534c2514a454f2d2
Joydev Gorai <119519363+Joy-Gorai@users.noreply.github.com>
2024-07-02 10:03:37 +0000

    Comparison of datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)            // avoid this
+console.log(null == 0)           // avoid this
+console.log(null >= 0)           // avoid this
+
+console.log(undefined == 0)      // avoid this
+console.log(undefined > 0)       // avoid this
+console.log(undefined < 0)       // avoid this
+
+// ===
+
+console.log("2" === 2)
